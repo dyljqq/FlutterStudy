cdf59f4783085748337dead037207dbdbabe7d1e
jiqinqiang <jiqinqiang@lukou.com>
2019-07-07 12:03:54 +0800

    add paint

--- sample_app/lib/main.dart
+++ sample_app/lib/main.dart
@@ -136,48 +136,48 @@ class SampleApp extends StatelessWidget {
 //   }
 // }
 
-class SignaturePainter extends CustomPainter {
-  SignaturePainter(this.points);
+// class SignaturePainter extends CustomPainter {
+//   SignaturePainter(this.points);
 
-  final List<Offset> points;
+//   final List<Offset> points;
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     var paint = Paint()
+//       ..color = Colors.black
+//       ..strokeCap = StrokeCap.round
+//       ..strokeWidth = 5.0;
     
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i], points[i + 1], paint);
-      }
-    }
-  }
+//     for (int i = 0; i < points.length - 1; i++) {
+//       if (points[i] != null && points[i + 1] != null) {
+//         canvas.drawLine(points[i], points[i + 1], paint);
+//       }
+//     }
+//   }
 
-  @override
-  bool shouldRepaint(SignaturePainter other) => other.points != points; 
-}
+//   @override
+//   bool shouldRepaint(SignaturePainter other) => other.points != points; 
+// }
 
-class Signature extends StatefulWidget {
-  SignatureState createState() => SignatureState();
-}
+// class Signature extends StatefulWidget {
+//   SignatureState createState() => SignatureState();
+// }
 
-class SignatureState extends State<Signature> {
-  List<Offset> _points = <Offset>[];
+// class SignatureState extends State<Signature> {
+//   List<Offset> _points = <Offset>[];
 
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onPanUpdate: (DragUpdateDetails details) {
-        setState(() {
-          RenderBox box = context.findRenderObject();
-          Offset position = box.globalToLocal(details.globalPosition);
-          _points = List.from(_points)..add(position);
-        });
-      },
-      onPanEnd: (DragEndDetails details) => _points.add(null),
-      child: CustomPaint(painter: SignaturePainter(_points), size: Size.infinite),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onPanUpdate: (DragUpdateDetails details) {
+//         setState(() {
+//           RenderBox box = context.findRenderObject();
+//           Offset position = box.globalToLocal(details.globalPosition);
+//           _points = List.from(_points)..add(position);
+//         });
+//       },
+//       onPanEnd: (DragEndDetails details) => _points.add(null),
+//       child: CustomPaint(painter: SignaturePainter(_points), size: Size.infinite),
+//     );
+//   }
+// }
