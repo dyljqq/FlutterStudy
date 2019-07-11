60cd68e15985c61d29a6a54430de0f75f238ff39
jiqinqiang <jiqinqiang@lukou.com>
2019-07-11 15:48:15 +0800

    add widget demo

---
+++ widget_demo/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyScaffold(),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(color: Colors.blue),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+
+}
+
+// 使用SafeArea自动适配刘海屛
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            MyAppBar(
+              title: Text(
+                'Example title',
+                style: Theme.of(context).primaryTextTheme.title
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: Text('Hello World'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
